79a6c76a8c1c00ab1629b4eeeae48a55e16aa660
anujasurve <96607901+anujasurve@users.noreply.github.com>
2024-05-17 12:38:49 +1200

    Add files via upload

---
+++ themes/custom/artists/js/slider.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * landscaping behaviors.
+ */
+// (function ($, Drupal) {
+//   Drupal.behaviors.count = {
+//     attach(context, settings) {
+//       var slider = tns({
+//         container: ".my-slider",
+//         items: 6,
+//         responsive: {
+//           640: {
+//             edgePadding: 20,
+//             gutter: 20,
+//             items: 2,
+//           },
+//           700: {
+//             gutter: 30,
+//           },
+//           900: {
+//             items: 3,
+//           },
+//         },
+//       });
+//     },
+//   };
+// })(jQuery, Drupal);
